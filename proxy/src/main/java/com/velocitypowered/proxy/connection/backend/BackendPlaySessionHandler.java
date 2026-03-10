0eb1fca04c38449250ad4db89b0f7ef4eff926c2
Preva1l <prod.preva1l@gmail.com>
2026-03-11 02:54:56 +1100

    feat: extra events

--- proxy/src/main/java/com/velocitypowered/proxy/connection/backend/BackendPlaySessionHandler.java
+++ proxy/src/main/java/com/velocitypowered/proxy/connection/backend/BackendPlaySessionHandler.java
@@ -300,9 +300,7 @@ public class BackendPlaySessionHandler implements MinecraftSessionHandler {
     }
 
     if (PluginMessageUtil.isMcBrand(packet)) {
-      PluginMessagePacket rewritten = PluginMessageUtil
-              .rewriteMinecraftBrand(packet,
-                      server.getVersion(), playerConnection.getProtocolVersion());
+      PluginMessagePacket rewritten = PluginMessageUtil.rewriteMinecraftBrand(packet, server, serverConn.getPlayer());
       playerConnection.write(rewritten);
       return true;
     }
